a7188316107c6c7f7a3897fa4a1916f1aa5f7490
Steve <edxteve@gmail.com>
2023-03-18 08:43:54 -0500

    Clase04 Completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => {
+
 const btn=document.querySelector('[data-form-btn]');
 
 const createTask=(event) =>{
@@ -10,20 +12,41 @@ const createTask=(event) =>{
     task.classList.add("card");
     input.value="";
 
-    //backticks
-    const content=`<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    //backticks y Crear Elementos
 
-    task.innerHTML=content;
-    list.appendChild(task);
+    const taskContent=document.createElement("div");
     
+    const titleTask=document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText=value;
 
-    console.log(task);
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+
+    //const content=`<i class="fas fa-trash-alt trashIcon icon"></i>`
+    //task.innerHTML=content;
+
+    task.appendChild(taskContent);
+    list.appendChild(task);    
+
+}
+
+const checkComplete= () =>{
+    const i=document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click", completeTask);
+    return i;
+}
+
+const completeTask= (event) =>{
+    const element=event.target;
+    element.classList.toggle('fas');//Clase importada de font Awsome 
+    element.classList.toggle('completeIcon');//Clase importada de CSS
+    element.classList.toggle('far');
 }
 
 //listener
 btn.addEventListener('click', createTask);
 
+
+})();
